2b56e3fdfab0907a50aa9dc9456d345922579192
Aditya Varma <adityavarma2106.pwap@gmail.com>
2023-04-06 17:52:03 +0530

    lab 4 q3 corrected

--- lab_3_3.c
+++ lab_3_3.c
@@ -5,12 +5,13 @@ int main()
     int calls;
     printf("Number of calls are: ");
     scanf("%d",&calls);
-    if(calls<=50){
-        bill=50;
-    }
-    else{
-        bill=250+(calls-50)*1.5;
-    }
+    bill=(calls<=50)?(250):(250+(calls-50)*1.5);
+    // if(calls<=50){
+    //     bill=250;
+    // }
+    // else{
+    //     bill=250+(calls-50)*1.5;
+    // }
     printf("%f",bill);
     return 0;
 }
